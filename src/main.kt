fa52da9ae344fd7dd51ed68c887fd5cf8350b902
Your Name <you@example.com>
2022-03-07 00:20:11 -0500

    first commit

---
+++ src/main.kt
@@ -0,0 +1,33 @@
+
+
+fun  main() {
+    var name= arrayOf("petre","mark","rode")
+    println(name.contentToString())
+
+
+
+   var numbers = arrayOf(1, 2, 4, 5, 6, 78, 7, 6, 556, 4)
+    var sortedNum=numbers.sortedArrayDescending()
+   println(sortedNum.contentToString())
+     numbers.forEach{ num ->
+
+
+
+
+   println(numbers.minOrNull())
+
+
+   var tableone=arrayOf("Tessa","Nyawera","Angela","Mereyan","Mercy","Naima")
+    var index =tableone.indexOf("Angela")
+    println(index)
+   var table2= arrayOf("Brenda","Caro","Daina")
+
+   tableone=table2.plus(tableone)
+  println(table2.contentToString())
+     }
+
+
+
+
+
+
